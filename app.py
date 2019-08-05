f026aa380af9a673a819f6b916b864a59cc6edaa
Jack Gay <jackpowis@gmail.com>
2019-08-05 17:09:30 +0100

    variables contd

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-
+# string
 character_name = "Tom"
-character_age = "50"
+
+# number
+character_age = 50
 
 print("There once was a man named " + character_name + ", ")
 print("he was " + character_age + " years old. ")
