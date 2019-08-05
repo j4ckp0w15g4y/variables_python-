74dbc16f7b3bc0330041c447e90001b0593847c1
Jack Gay <jackpowis@gmail.com>
2019-08-05 16:55:33 +0100

    playing with variables

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-print("There once was a man named George, ")
-print("he was 70 years old. ")
-print("He really liked the name George, ")
-print("but didn't like being 70.")
+
+character_name = "Tom"
+character_age = "50"
+
+print("There once was a man named " + character_name + ", ")
+print("he was " + character_age + " years old. ")
+
+character_name = "Mike"
+
+print("He really liked the name " + character_name + ", ")
+print("but didn't like being " + character_age + ".")
